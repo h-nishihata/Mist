d5aa93fb9981d73379131440a41ad12124076602
h-nishihata <nishihata0301@gmail.com>
2019-09-04 11:50:53 +0900

    initially added

---
+++ ledSwitcher.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import tkinter as tk
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+
+def ledOn(event):
+    GPIO.output(21, GPIO.HIGH)
+    print('ON')
+
+def ledOff(event):
+    GPIO.output(21, GPIO.LOW)
+    print('OFF')
+
+def quitApp(event):
+    GPIO.cleanup()
+    import sys; sys.exit()
+
+
+root = tk.Tk()
+frame = tk.Frame()
+root.title('Switcher')
+
+widget00 = tk.Button(frame, text = 'ON')
+widget00.bind('<Button-1>', ledOn)
+widget00.pack(padx = 72, pady = 24)
+
+widget01 = tk.Button(frame, text = 'OFF')
+widget01.bind('<Button-1>', ledOff)
+widget01.pack(padx = 72, pady = 12)
+
+widget02 = tk.Button(frame, text = 'QUIT')
+widget02.bind('<Button-1>', quitApp)
+widget02.pack(padx = 72, pady = 48)
+
+frame.pack()
+root.mainloop()
+GPIO.cleanup()
